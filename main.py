13d7fd26ba3a181ac9a072334ae1e898ab04f9ee
Xin He <xinhe28-c@my.cityu.edu.hk>
2019-11-21 23:08:31 +0800

    loop year

--- main.py
+++ main.py
@@ -1,68 +1,9 @@
 from event import *
-from datetime import datetime
+from loop_year import *
 
-import os
-os.getcwd()
-os.chdir('./xml-data/2002/')
-print(os.getcwd())
 
-tb = pd.DataFrame()
-n=0
-for file in os.listdir('./'):
-    n=n+1
-    print(n)
-    if file.endswith(".xml"):
-        print(file)
-        # file_name = file[:-4]
-        # file_name = '92800_T'
-        e1 = event(file)
-        e1.get_elements()
-        tb[e1.id] = e1.tab()
+for year in range(2001,2020):
+    print(year)
+    loop_year(year)
 
-tb.index = ['xmleventtypeid','xmlticker','xmleventdate','xmlfakeqtr','xmlcity','xmlcompanyname']
-tb = tb.T
-
-#dates = [datetime.strptime(i[:-6], '%d-%b-%y %H:%M') for i in d['eventdate']]
-
-## data format
-dates = pd.to_datetime(tb['xmleventdate'])
-tb['xmlymd'] = [i.strftime('%Y%m%d') for i in dates]
-
-## ticker, some are '\n'
-tic = []
-for i in tb['xmlticker']:
-    t = i
-    if i[0]=='\n':
-        t='NAN'
-    tic.append(t)
-
-tb['xmlticker'] = tic
-
-## city, some are '\n'
-city = []
-for i in tb['xmlcity']:
-    t = i
-    if i[0]=='\n':
-        t='NAN'
-    city.append(t)
-
-tb['xmlcity'] = city
-
-tb.to_csv('../xml-summary/2002xml.csv')
-print('finish')
-
-#
-#        txt = read_pdf(file_name)
-#        counter = tokenize(file_name,txt)
-#        df = pd.DataFrame.from_dict(counter, orient='index').reset_index()
-#        df.columns = ['token','count']
-#        df = df.sort_values('count',ascending=False)
-#        os.chdir('../')
-#        os.chdir('../')
-#        df.to_csv('./tokenization/PDF2001/'+file_name+'.csv', header=True, index=False)
-#        os.getcwd()
-#        os.chdir('./conference call/PDF2001/')
-#        os.getcwd()
-
-        
-
+print('loop finish')
