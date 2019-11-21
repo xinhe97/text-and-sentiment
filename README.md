1ef106c33fe1966656713507239bc0aee34773c1
Xin He <xinhe28-c@my.cityu.edu.hk>
2019-11-22 00:35:21 +0800

    merge all year

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # text-and-sentiment
 private
+
+
+
+1. text data
+2. Python: read text data and process --> csv
+3. WRDS cloud: merge csv with compustat
--- main.py
+++ main.py
@@ -7,3 +7,13 @@ for year in range(2001,2020):
     loop_year(year)
 
 print('loop finish')
+
+print('merge each year to one file')
+
+da = pd.DataFrame()
+for i in range(2001,2020):
+    tb = pd.read_csv('./xml-data/xml-summary/{}xml.csv'.format(i))
+    da = da.append(tb)
+    
+da.to_csv('./xml-data/xml-summary/all-year-xml.csv')
+
